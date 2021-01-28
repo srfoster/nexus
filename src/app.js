37283d93f383f612ddc107c1db9b121fe208d790
Justin Foster <jfoster91@gmail.com>
2021-01-28 16:45:37 -0700

    Post is working

--- src/app.js
+++ src/app.js
@@ -14,12 +14,55 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json());
 
 // epStart = EndPointStart, this automatically changes the server test
 const epStart = '/';
 const startupGreet = 'Welcome aboard Captain, all systems online.';
 app.get(epStart, (req, res) => {
   res.send(startupGreet)
+})
+
+app.get('/play', (req, res) => {
+  req.app.get('db')('game_states')
+    .where({})
+    .then((gameStates) => {
+      res.send({
+        secretTarget: secret_target, 
+        setTarget: target_word,
+        lettersGuessed: letters_guessed,
+      })
+    })
+})
+
+app.post('/play', (req, res) => {
+  console.log(req.body);
+  req.app.get('db')('game_states')
+  .where({})
+  .then((gameStates) => {
+    if(gameStates.length === 0){
+      req.app.get('db')('game_states').insert({
+        letters_guessed: JSON.stringify(req.body.lettersGuessed),
+        secret_target: req.body.secretTarget,
+        target_word: req.body.targetWord
+      })
+      .returning('*')
+      .then(game_state => {
+        res.send({message: "Message received"})
+      })
+    } else{
+      req.app.get('db')('game_states').where({id: gameStates[0].id}).update({
+        letters_guessed: JSON.stringify(req.body.lettersGuessed),
+        secret_target: req.body.secretTarget,
+        target_word: req.body.targetWord
+      })
+      .returning('*')
+      .then(game_state => {
+        res.send({message: "Message received"})
+      })
+    }
+
+  })
 })
 
 app.use(function errorHandler(error, req, res, next) {
