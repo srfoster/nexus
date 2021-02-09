45875eadbd51562cbce3aa6ce3ef0f53213f458e
Justin Foster <jfoster91@gmail.com>
2021-02-09 16:26:25 -0700

    Spell:id endpoint is working

---
+++ src/toDo.js
@@ -0,0 +1,11 @@
+/*
+  Make text not centered in codemirror
+  Add codemirror syntax highlighting for scheme
+
+  Make back-end tests pass
+  Edits in codemirror should post and be saved in db
+  Make sure spell id route with codemirror is protected
+
+  Write tests to make sure codemirror endpoint requires auth
+
+*/
