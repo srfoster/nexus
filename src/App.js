5422d92ce43591dcb372d5fafdf25268619e3be9
Trithir <ericstevens10@gmail.com>
2021-03-05 15:16:47 -0800

    try 1

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import SpellDetails from './Components/Dashboard/SpellDetails';
 import Dashboard from './Components/Dashboard/Dashboard';
 import PublicSpells from './Components/PublicSpells';
-import UserProfile from './Components/UserProfile';
+// import UserProfile from './Components/UserProfile';
 require('codemirror/mode/scheme/scheme');
 
 function App() {
@@ -42,18 +42,15 @@ function App() {
             path={'/spells'}
             component={SpellIndex}
           />
-          <Route 
+          <Route
             path={'/friends'}
             component={(props) => <Dashboard><div>Friends Coming Soon</div></Dashboard>}
           />
-          <Route 
+          <Route
             path={'/gallery'}
             component={(props) => <Dashboard><PublicSpells /></Dashboard>}
           />
-          <Route
-            path={'/profile/:id'}
-            component={(props) => <Dashboard><UserProfile /></Dashboard>}
-          />
+
         </Switch>
       </div>
     </div>
