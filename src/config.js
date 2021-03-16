77ffe94000eeeec398ac3abe4cf20f5318bb6fdc
Justin Foster <jfoster91@gmail.com>
2021-03-16 15:16:01 -0600

    Deployment prep

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
   DATABASE_URL: process.env.DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET || 'super-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
 }
