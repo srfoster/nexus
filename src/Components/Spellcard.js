d920a741b96042455f343889bff382fd2e1100fc
Trithir <ericstevens10@gmail.com>
2021-03-16 10:57:49 -0700

    Added tooltip

--- src/Components/Spellcard.js
+++ src/Components/Spellcard.js
@@ -33,6 +33,10 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import {UnControlled as CodeMirror} from 'react-codemirror2';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const Spellbook = (props) => {
   const classes = useStyles();
@@ -97,7 +101,11 @@ const Spellbook = (props) => {
             <ShareIcon />
           </IconButton> */}
           <IconButton onClick={() => clickForkIcon(props.spell.id)}>
-            <CallSplitIcon />
+              <Tooltip title="Fork Spell" placement="top">
+                <IconButton aria-label="Fork Spell">
+                  <CallSplitIcon />
+                </IconButton>
+              </Tooltip>
             {/* <svg class="octicon octicon-repo-forked" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg> */}
           </IconButton>
           <IconButton
@@ -108,7 +116,11 @@ const Spellbook = (props) => {
             aria-expanded={expanded}
             aria-label="show more"
           >
-            <CodeIcon />
+          <Tooltip title="View Code" placement="top">
+            <IconButton aria-label="View Code">
+              <CodeIcon />
+            </IconButton>
+          </Tooltip>
           </IconButton>
         </CardActions>
 
