a2f83c275552a8aa0731781162c7b0af06f0b356
Trithir <ericstevens10@gmail.com>
2021-04-22 10:23:50 -0700

    Quick n' dirty drawer fix

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -17,7 +17,8 @@ import clsx from 'clsx';
 import CodeSpells from '../../Assets/CodeSpells.png';
 import Link from '@material-ui/core/Link';
 import SpellsApiService from '../../Services/spells-api-service';
-import useStyles from '../../styles.js';
+// import useStyles from '../../styles.js';
+import { makeStyles} from '@material-ui/core/styles';
 
 function Dashboard(props) {
   const classes = useStyles();
@@ -150,5 +151,94 @@ function Dashboard(props) {
   </div>
   )
 }
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  dashRoot: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  dashMenuButton: {
+    marginRight: 36,
+  },
+  dashMenuButtonHidden: {
+    display: 'none',
+  },
+  dashTitle: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,},
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  dashContainer: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  dashPaper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+  link: {
+    width: '10%',
+  }
+}));
 
 export default Dashboard;
