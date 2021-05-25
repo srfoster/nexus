82a93ef8df831800c4875accc70b338a5ba9de83
srfoster <lindseyhandley@gmail.com>
2021-05-25 16:53:48 -0700

    started outlining a few pages

--- frontend/src/Components/Docs/Docs.js
+++ frontend/src/Components/Docs/Docs.js
@@ -7,20 +7,56 @@ const topDocLink = linkTo("docs", "Back to Top")
 
 const docPageContent = {
  docs: <>
-        <p>Welcome to the Nexus! [Say more about what the Nexus is...]</p>
-	<p>If it's your first time here, we recommend: {linkTo("getting started", "Getting Started")}</p>
-        <p>There are two CodeSpells worlds in the Nexus right now.  You can read more about those here:</p>
+        <p>Welcome to the Nexus!</p>
+        <p>The Nexus is a hub for the CodeSpells community where we can write, save, share, and find spells that can be cast in game, 
+          download CodeSpells authored works, and connect with other Mages.</p>
+	      <p>If it's your first time here, we recommend beginning your journey in our {linkTo("getting-started", "Getting Started")} section.</p>
+        <p>There are two CodeSpells worlds in the <a href="/downloads">Downloads</a> section right now.  You can read more about those here, as well as learn how to create your own CodeSpells authored works:</p>
         <ul>
           <li>{linkTo("orb-world", "Orb World")}</li>
           <li>{linkTo("orb-lab", "Orb Lab")}</li>
+          <li>{linkTo("building-new-games", "Building New Games")}</li>
         </ul>
-        <p>If you're just looking for the spell-crafting APIs, here they are: {linkTo("langs", "APIs")}</p>
+        <p>If you just want to write spells and you're looking for the spell-crafting APIs, you can find them here: {linkTo("langs", "APIs")}</p>
         <p>Above all, CodeSpells is a community, and the Nexus is the hub we are building for ourselves.  Learn more about the people involved in the community here: {linkTo("people", "People")}.</p>
       </>,
- "orb-world": <>{topDocLink}<p>Wanna know about the Orb World?</p></>,
- "orb-lab": <>{topDocLink}<p>What is Orb Lab?  Many have asked.  Few know.</p></>,
- "langs": <>{topDocLink}<p>Here are the APIs...</p></>,
- "getting started": <>{topDocLink}<p>Gotta start somewhere!</p></>,
+  "orb-world": <>
+    {topDocLink}
+    <h2>About</h2>
+    <p>Wanna know about the Orb World?</p>
+    <h2>Installation</h2>
+    <h2>Getting Started</h2>
+    <h2>What's Next?</h2>
+  </>,
+  "orb-lab": <>
+    {topDocLink}
+    <h2>About</h2>
+    <p>What is Orb Lab?  Many have asked.  Few know.</p>
+    <h2>Installation</h2>
+    <h2>Getting Started</h2>
+    <h2>What's Next?</h2>
+  </>,
+  "langs": <>
+    {topDocLink}
+    <p>Each authored work has its own API. You can find the APIs of the currently available Authored Works here:</p>
+    <ul>
+      <li>Orb World - API coming soon</li>
+      <li>Orb Lab - API coming soon</li>
+    </ul>
+    <p>You can find the API for the <a href="https://www.twitch.tv/codespells" target="_blank">Twitch</a> chat here: Twitch Chat - API coming soon</p>
+  </>,
+  "getting-started": <>
+    {topDocLink}
+    <p>Gotta start somewhere!</p>
+  </>,
+  "people": <>
+    {topDocLink}
+    <p>Our community is amazing!</p>
+  </>,
+  "building-new-games": <>
+    {topDocLink}
+    <p>Want to know how to build your own CodeSpells games?</p>
+</>,
 }
 
 function Docs(props) {
