4c8596a7f7876da13a75e4555e557cbb4617bd83
Lindsey Handley <lindseyhandley@gmail.com>
2021-05-27 16:11:15 -0700

    created a simple test, but i don't understand why it's not passing...

---
+++ frontend/src/Components/tests/Docs.test.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import Docs from '../Docs/Docs';
+import { render, fireEvent, waitFor, screen} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { MemoryRouter, Route, useParams, useRouteMatch } from "react-router";
+
+describe('When Docs first renders', () => {
+    beforeEach(() => {
+        render(
+        <MemoryRouter>
+            <Docs match={{params: {page: "docs"}}} />
+        </MemoryRouter>
+      )
+    });
+    it('renders without crashing', () => {});
+    it('shows docs page header' , () => {
+      expect(screen.getByText('Docs')).toBeInTheDocument()
+    });
+    it('shows docs header' , () => {
+      expect(screen.getAllByRole('heading')).toContain([<h1>Docs</h1>])
+    });
+});
