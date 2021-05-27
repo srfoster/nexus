ae6606428fe7a4caaa51431494100d663baeb286
Stephen Foster <stephen@thoughtstem.com>
2021-05-27 12:46:06 -0700

    indicator ui

--- frontend/src/Components/Client/ConnectionIndicator.js
+++ frontend/src/Components/Client/ConnectionIndicator.js
@@ -1,25 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import CheckIcon from '@material-ui/icons/Check';
+import Chip from '@material-ui/core/Chip';
+
+const tryToConnect = (onSuccess) => {
+    let s = new WebSocket("ws://localhost:8082/test");
+    s.onerror = function (event) {
+        onSuccess()
+        console.log("Websocket connection failed, retrying...", event)
+        setTimeout(() => tryToConnect(onSuccess), 1000)
+    }
+    s.onmessage = function (event) {
+        onSuccess()
+    }
+    s.open = function (event) {
+        console.log("Open...");
+    }
+    window.CodeSpellsSocket = s;
+}
+
+const NotConnected = (props) => {
+    return <>
+        
+        <Chip
+            icon={<CircularProgress color="secondary" />}
+            label={"Connecting"}/>
+    </>
+}
+
+
+const Connected = (props) => {
+    return <>
+        <Chip
+            icon={<CheckIcon />}
+            label={"Connected!"}/>
+        {props.afterConnection}
+    </>
+}
 
 const ConnectionIndicator = (props) => {
     const [connected, setConnected] = useState(false)
 
     useEffect(() => {
-        var s = new WebSocket("ws://localhost:8082/test");
-        s.onmessage = function (event) {
-            if (!connected)
-                setConnected(true)
-        }
-        s.open = function (event) {
-            console.log("Open...");
-        }
-        window.CodeSpellsSocket = s;
+      tryToConnect(()=>setConnected(true))
     },[])
 
-    return (
-    <>
-            {connected ? <><p>Connected</p>{props.afterConnection}</> : "Not Connected"}
+    return <>
+        {connected ? <Connected afterConnection={props.afterConnection} /> : <NotConnected />}
     </>
-  )
 };
 
 export default ConnectionIndicator;
