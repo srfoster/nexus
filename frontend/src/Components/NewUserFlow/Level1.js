7fd14d375697e02c95baa7a0acf4d17fb1529307
Stephen Foster <stephen@thoughtstem.com>
2021-06-09 15:59:00 -0700

    redo 1st video

--- frontend/src/Components/NewUserFlow/Level1.js
+++ frontend/src/Components/NewUserFlow/Level1.js
@@ -3,6 +3,8 @@ import { useHistory } from "react-router-dom";
 import { Level, ShowAfter } from "./Level";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Fade from '@material-ui/core/Fade';
 import ReactPlayer from 'react-player'
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
@@ -16,7 +18,58 @@ const GatewayVideo = (props) => {
   const [showMakeAccount, setShowMakeAccount] = useState(false)
   const [started, setStarted] = useState(false)
   const [playing, setPlaying] = useState(false)
+  const [time, setTime] = useState(0)
   const [activeStep, setActiveStep] = useState(0)
+
+  let signupBit = []
+
+  if (time >= 5) {
+    signupBit = [< SignupForm
+      history={history}
+      showTopContent={false}
+      signupButtonContent={"Make Account"}
+    ></SignupForm>]
+  }
+  if (time >= 21) {
+    signupBit = []
+    signupBit.push(<Fade in={true} timeout={500}><h1>Hey...</h1></Fade>)
+  }
+  if (time >= 24) {
+    signupBit.push(<h2>Yeah, you.</h2>)
+  }
+    
+  if (time >= 29) {
+    signupBit = []
+    signupBit.push(<Fade in={true} timeout={500}><h1>Did you see that?</h1></Fade>)
+  }
+
+  if (time >= 30) {
+    signupBit.push(<h2>Cool, huh?</h2>)
+  }
+
+  if (time >= 31) {
+    signupBit.push(<h2>I can write stuff.</h2>)
+  }
+
+  if (time >= 32) {
+    signupBit.push(<h2>Over on the side.</h2>)
+  }
+
+  if (time >= 37.5) {
+    signupBit = []
+  }
+
+  if (time >= 38.5) {
+    signupBit = [< SignupForm
+      history={history}
+      showTopContent={false}
+      signupButtonContent={"Make Account"}
+    ></SignupForm>]
+  }
+
+  if (time >= 45) {
+    signupBit.unshift(<h3 style={{textAlign: "center"}}>(Please)</h3>)
+  }
 
   return (
     <>
@@ -33,36 +86,39 @@ const GatewayVideo = (props) => {
       <Grid container spacing={3}>
         <Grid item xs={6}>
           <Card>
-          <h2>Can you figure out how to
+            <CardHeader title={
+              <span style={{fontSize: 16}}> Can you figure out how to
         <span style={{ cursor: "pointer" }}
-              onClick={() => {
-                setPlaying(!playing);
-                setStarted(true);
-                //setActiveStep(1)
-              }}> {playing ? " pause " : " play "} </span>
-           the video?</h2>
-          <ReactPlayer
-            url="https://codespells-org.s3.amazonaws.com/NexusVideos/e1.mp4"
-            playing={playing}
-            progressInterval={100}
-            onProgress={(p) => {
-              if (!showMakeAccount && p.playedSeconds >= 5) {
-                setShowMakeAccount(true)
-                setActiveStep(activeStep + 1)
-              }
-            }}
-          />
+                  onClick={() => {
+                    setPlaying(!playing);
+                    setStarted(true);
+                    //setActiveStep(1)
+                  }}> {playing ? " pause " : " play "} </span>
+           the video?</span>}></CardHeader>
+            <ReactPlayer
+              url="https://codespells-org.s3.amazonaws.com/NexusVideos/e1-sock.mp4"
+              style={{
+    "transform": "rotateY(180deg)",
+    "-webkit-transform": "rotateY(180deg)",
+    "-moz-transform": "rotateY(180deg)"
+}}
+              playing={playing}
+              progressInterval={100}
+              onProgress={(p) => {
+                setTime(p.playedSeconds)
+                if (!showMakeAccount && p.playedSeconds >= 5) {
+                  setShowMakeAccount(true)
+                  setActiveStep(activeStep + 1)
+                }
+              }}
+            />
           </Card>
         </Grid>
 
         <Grid item xs={6}>
           <Card style={{height: "100%"}}>
             <CardContent>
-              {showMakeAccount ? <SignupForm
-                history={history}
-                showTopContent={false}
-                signupButtonContent={"Make Account"}
-              ></SignupForm> :
+              {showMakeAccount ? signupBit :
                 <Avatar
                   onClick={(e) => {
                     if(e.ctrlKey)
@@ -80,9 +136,18 @@ const GatewayVideo = (props) => {
   )
 }
 
+/*
+      <ul>
+        <li>Goal of our THING</li>
+        <li>THING: Edutainment experience</li>
+        <li>Why it's valuable to you</li>
+        <li>Why we're doing it</li>
+        <li>Create an account to continue</li>
+      </ul>
+*/
 function Level1(props) {
     return(<Level setBadges={props.setBadges} number={1} subtitle={"At the threshold"}>
-        <GatewayVideo />
+      <GatewayVideo />
     </Level>)
 }
 
