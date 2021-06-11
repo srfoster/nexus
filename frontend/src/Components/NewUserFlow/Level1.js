b6ef79d1265160e51b73b74db9b45c61ac275994
Stephen Foster <stephen@thoughtstem.com>
2021-06-11 14:10:27 -0700

    after video questions

--- frontend/src/Components/NewUserFlow/Level1.js
+++ frontend/src/Components/NewUserFlow/Level1.js
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from "react-router-dom";
-import { Level, ShowAfter } from "./Level";
+import { Level } from "./Level";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import Fade from '@material-ui/core/Fade';
 import ReactPlayer from 'react-player'
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import useStyles from '../../styles.js';
-import SignupForm from '../../Components/SignupForm';
 import DarkModeSwitch from '../Widgets/DarkModeSwitch';
 import TextField from '@material-ui/core/TextField';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -23,6 +17,8 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormLabel from '@material-ui/core/FormLabel';
+import Chip from '@material-ui/core/Chip';
+
 
 const ContinueButton = (props) => {
   return (
@@ -135,7 +131,8 @@ const MeetYourTeacher = (props) => {
           { correct: true, text: "Sock Puppet", feedback: "It's available!" },
           { correct: false, text: "A Wizard from the Forest", feedback: "Sorry, your current level is too low to unlock this teacher." },
           { correct: false, text: "Super Intelligent AI", feedback: "Sorry, your current level is too low to unlock this teacher." },
-          { correct: false, text: "The Creators of this EdTech Software", feedback: "Sorry, your current level is too low to unlock this teacher." },
+          { correct: false, text: "The Creators of nexus.codespells.org", feedback: "Sorry, your current level is too low to unlock this teacher." },
+          { correct: false, text: "None of these", feedback: "Sorry, your current level is to low for you to continue without a teacher." },
         ]}
           buttonText="Check Availability"
           onCorrect={()=>setTeacherAvailable(true)}
@@ -153,15 +150,13 @@ const MeetYourTeacher = (props) => {
 
     return (
       <SBS
-        leftSideTitle={<>
-          <p>Congratulations, you've unlocked Sock Puppet!</p>
-        </>}
+        leftSideTitle={<>You've unlocked:<Chip avatar={<Avatar alt="Natacha" src="/static/images/avatar/1.jpg" />} label="Sock Puppet"></Chip></>}
         leftSide={
           <>
             <ReactPlayer
-            playsInline
-            fluid={false}
-        width={"100%"}
+              playsInline
+              fluid={false}
+              width={"100%"}
               url="https://codespells-org.s3.amazonaws.com/NexusVideos/e1-sock-2.mp4"
               controls={true}
               style={{}}
@@ -174,7 +169,6 @@ const MeetYourTeacher = (props) => {
         }
         rightSide={videoFinished ?
           <><WhyIsMyTeacherASock />
-            <ContinueButton onClick={console.log("Finished!")}/>
           </> : ""}
       />
     );
@@ -183,19 +177,40 @@ const MeetYourTeacher = (props) => {
   // Decision: Do we need this question? What purpose does it serve?
   // Pro: breaks up video content, nice gamification rhythm
   function WhyIsMyTeacherASock(props) {
-    const [response, setResponse] = React.useState(false);
-    return (<MultipleChoiceQuestion
-      question="Do you think a sock can teach?"
-      answers={[
-          { correct: true, text: "Yes, if a sock can't teach something, it cannot be taught!", feedback: "Personality assessment result: User with name \"" + username + "\" is an optimist who like socks." },
-          { correct: true, text: "Yes, socks can teach you what not to do.", feedback: "" },
-          { correct: true, text: "No, socks are inanimate objects.", feedback: "" },
-          { correct: true, text: "No, ...", feedback: "" },
-        ]}
+    const [answerChosen, setAnswerChosen] = React.useState(false);
+    return (
+      <>
+        <MultipleChoiceQuestion
+          question="Your feelings matter.  Do you think a sock can teach?"
+          answers={[
+            {
+              correct: true, text: "Yes, if a sock can't teach something, it cannot be taught!",
+              feedback: `Personality assessment result: User with name "${username}" is an optimist who like socks.  User may continue.`
+            },
+            {
+              correct: true, text: "Yes, socks can teach you what NOT to do.",
+              feedback: `Personality assessment result: User with name "${username}" harbors mild anti-sock tendencies and should be monitored.  User may continue.`
+            },
+            {
+              correct: true, text: "No, socks are inanimate objects",
+              feedback: `Personality assessment result: User with name "${username}" considers themselves to be far superior to socks.  Recommended for sock sympathy training.  User may continue.`
+            },
+            {
+              correct: true, text: "No, I do not trust socks",
+              feedback: `Personality assessment result: User with name "${username}" has likely had bad experiences with socks in the past.  Recommended for sock exposure therapy.  User may continue.`
+            },
+            {
+              correct: false, text: "These answers are too restrictive",
+              feedback: `Personality assessment result: User with name "${username}" is a narcisist who thinks their special snowflake-like personality is too complex to be assessed by multiple-choice questions. User may not continue.`
+            },
+          ]}
           buttonText="Submit Answer"
-          onCorrect={()=>setTeacherReflectionDone(true)}
-          onIncorrect={()=>setTeacherReflectionDone(false)}
-    />)
+          onCorrect={() => setAnswerChosen(true)}
+          onIncorrect={() => setAnswerChosen(false)}
+        />
+        {answerChosen ? <ContinueButton onClick={ () => setTeacherReflectionDone(true)} /> : ""}
+      </>
+    )
   }
   
   function Level1CompleteScreen(props) {
