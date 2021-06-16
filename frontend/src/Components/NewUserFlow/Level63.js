7301b6e60fda020c6db344e1bd245ad4c55b1c12
Justin <jfoster91@gmail.com>
2021-06-16 18:28:56 +0000

    It's broken again

---
+++ frontend/src/Components/NewUserFlow/Level63.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import App from './Sudoku/src/App'
+
+function Level63(props) {
+  return (
+    <div>
+      {/* <App></App> */}
+    </div>
+  );
+}
+
+export default Level63;
