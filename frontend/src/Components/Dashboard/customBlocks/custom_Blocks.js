11624fec64e60c25a607dea3a4054824e4cbd70e
Carlos <ccherrera01@gmail.com>
2021-06-16 14:21:34 -0700

    added blockly component and refactored custom_blocks

--- frontend/src/Components/Dashboard/customBlocks/custom_Blocks.js
+++ frontend/src/Components/Dashboard/customBlocks/custom_Blocks.js
@@ -1,120 +1,10 @@
 import Blockly from 'blockly';
-import 'blockly/python';
 
-var AtomsJson = {
-    "message0": "text input: %1",
-    "args0": [
-      {"type": "input_value", 
-      "name": "FIELDNAME", 
-      "variable": "text",},
-      
-    ],
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230
-  };
-
-Blockly.Blocks['atoms'] = {
-    init: function() {
-      this.jsonInit(AtomsJson);
-      // Assign 'this' to a variable for use in the tooltip closure below.
-      var thisBlock = this;
-      this.setTooltip(function() {
-        return 'Add a number to variable "%1".'.replace('%1',
-            thisBlock.getFieldValue('VAR'));
-      });
-    }
-  };
-
-  var reactDateField = {
-    "type": "test_react_date_field",
-    "message0": "date field %1",
-    "args0": [
-      {
-        "type": "field_react_date",
-        "name": "DATE",
-        "date": "01/01/2020"
-      },
-    ],
-    "previousStatement": null,
-    "nextStatement": null,
-  };
-  
-  Blockly.Blocks['test_react_date_field'] = {
-    init: function() {
-      this.jsonInit(reactDateField);
-      this.setStyle('loop_blocks');
-    }
-};
-
-var defineBlock = {
-  "type": "define",
-  "message0": "define function %1",
-  "args0": [
-    {
-      "type": "field_input",
-      "name": "DATE",
-      "text": "define"
-    },
-    {
-      "type": "input_statement",
-      "name": "NAME"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 230,
-};
-
-
-//Define Blocks
-Blockly.Blocks['define'] = {
-  init: function() {
-    this.appendStatementInput("test")
-        .setCheck(null)
-        .appendField(new Blockly.FieldTextInput("define"), "atom");
-    this.setPreviousStatement(false, null);
-    this.setNextStatement(false, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-Blockly.JavaScript['define'] = function(block) {
-  // Generate JavaScript for moving forward or backwards.
-  var value = block.getFieldValue('VALUE');
-  var code = '(' + value + ')';
-  return code;
-};
-
-
-//list blocks
-Blockly.Blocks['paren'] = {
-  init: function() {
-    this.appendStatementInput("NAME")
-        .setCheck(null);
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
-  this.setTooltip("");
-  this.setHelpUrl("");
-  }
-};
-
-Blockly.JavaScript['paren'] = function(block) {
-  // Generate JavaScript for moving forward or backwards.
-  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
-  var code = '(' + statements_name + ')';
-  return code;
-};
-
-//ATOM Block
-Blockly.Blocks['atom'] = {
+//Deanchor
+Blockly.Blocks['deanchor'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldTextInput("atom"), "NAME");
+        .appendField("Deanchor")
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
@@ -123,10 +13,151 @@ Blockly.Blocks['atom'] = {
   }
 };
 
-Blockly.JavaScript['atom'] = function(block) {
-  // Generate JavaScript for moving forward or backwards.
-  var text_name = block.getFieldValue('NAME');
-  var code =  text_name;
+Blockly.JavaScript['deanchor'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '(deanchor ' + ')';
+  return code;
+};
+ 
+//IF STATEMENTS
+Blockly.Blocks['if_statement'] = {
+  init: function() {
+    this.appendStatementInput("test-expr")
+        .setCheck(null)
+        .appendField("If");
+    this.appendStatementInput("then-expr")
+        .setCheck(null)
+        .appendField("then");
+    this.appendStatementInput("else-expr")
+        .setCheck(null)
+        .appendField("else");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['if_statement'] = function(block) {
+  var value_test_expr = Blockly.JavaScript.statementToCode(block, 'test-expr');
+  var statements_then_expr = Blockly.JavaScript.statementToCode(block, 'then-expr');
+  var statements_else_expr = Blockly.JavaScript.statementToCode(block, 'else-expr');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '(if ' + value_test_expr + ' ' + statements_then_expr + ' ' + statements_else_expr + ' )';
   return code;
 };
 
+//COLOR BLOCKS DROPDOWN MENU  
+Blockly.Blocks['color_drop'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Color")
+        .appendField(new Blockly.FieldDropdown([["Green","GREEN"], ["Blue","BLUE"], ["Yellow","YELLOW"], ["Red","RED"], ["Orange","ORANGE"], ["Teal","TEAL"]]), "NAME");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(30);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['color_drop'] = function(block) {
+  // Generate JavaScript for moving forward or backwards.
+  var value = block.getFieldValue('NAME');
+  var code = '(color ' + '"' + value + '"' + ')'
+  return code;
+};
+
+//Function builds DummyInputBlocks
+//string, [list of types]
+let defineRacketBlock = (blockName, inputs) => {
+  //stores references to input names
+  var nameList = [];
+  Blockly.Blocks[blockName] = {
+    init: function(state) {
+      let input = this.appendDummyInput()
+      .appendField(blockName)
+      nameList = [];    
+      for (let i = 0; i < inputs.length; i++){
+        const name = "Name" + i;  
+        input.appendField(new Blockly.FieldTextInput(), name);
+        nameList.push(name);
+            
+      }
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blockly.JavaScript[blockName] = function(block) {    
+    var values = nameList.map((nameList) => block.getFieldValue(nameList))
+    var code = '(' + blockName + ' ' //+  ' "' + values  + '"' + ')'
+    for (let i = 0; i < nameList.length; i++){
+      if (isNaN(values[i])){
+          code += "'" + values[i] + "'";
+          code += " ";
+      }
+      else {
+        code +=  values[i];
+        code += " ";
+        }
+    }
+    code += ')'
+    return code;
+  };
+}
+
+//STATEMENT BLOCKS
+let defineStatementRacketBlock = (blockName, inputs) => {
+  var nameList = [];
+  Blockly.Blocks[blockName] = {
+    init: function(state) {
+      nameList = [];    
+      for (let i = 0; i < inputs.length; i++){
+        const name = "Name" + i;
+        nameList.push(inputs[i]);
+        console.log(name)
+        this.appendStatementInput(name)  
+          .appendField(inputs[i]);     
+      }
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blockly.JavaScript[blockName] = function(block) {
+    // Generate JavaScript for moving forward or backwards.
+    //var values = nameList.map((nameList) => Blockly.JavaScript.statementToCode(block, nameList))
+    var code = '(' //+  ' "' + values  + '"' + ')'
+    for (let i = 0; i < nameList.length; i++){
+      const r = "Name" + i;
+      code +=  Blockly.JavaScript.statementToCode(block, r)
+      code += " ";
+    }
+    code += ')'
+    return code;
+  };
+
+}
+
+
+
+
+
+
+//Dummy Blocks, top-bottom connection
+defineRacketBlock("color", ["string"]);
+defineRacketBlock("force", ["number", "number", "number"]);
+defineRacketBlock("anchor", ["string"]);
+defineRacketBlock("atom", ["anchor"]);
+
+//Bracket blocks, no displayed text
+//defineStatementRacketBlock("parent",[" ", " "]);
+defineStatementRacketBlock("paren",[" "])
