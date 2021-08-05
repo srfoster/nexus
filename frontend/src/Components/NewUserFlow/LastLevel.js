7d43c543a73201491e5526dc58e8810f17d06b44
Lindsey Handley <lindseyhandley@gmail.com>
2021-08-05 14:41:58 -0700

    new changes

--- frontend/src/Components/NewUserFlow/LastLevel.js
+++ frontend/src/Components/NewUserFlow/LastLevel.js
@@ -1,16 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useLocalStorage, spread } from "../../Util";
 import Button from '@material-ui/core/Button';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import { ContinueButton } from './Level';
 import { Level } from './Level';
+import { NewMessageNotification, OpenedMessage, NexusDevsChip, StudentChip } from '../Widgets/NexusVoice';
+import Typography from '@material-ui/core/Typography';
 
 function LastLevelContent(props) {
-  return (
-    <>
-      <p>Hello!</p>
-    </>
+  const openedMessage = useRef(null);
+  var [messageOpened, setMessageOpened] = useLocalStorage("sock-puppet-last-lesson", false)
+  
+  return (!messageOpened ? <NewMessageNotification
+    nexusSays={"Wow!  New messages(s)..."}
+    from={<NexusDevsChip></NexusDevsChip>}
+    onOpenClicked={
+      () => {
+        setMessageOpened(true)
+      }
+    }
+  /> :
+    <div ref={openedMessage}>
+      <OpenedMessage
+        from={<NexusDevsChip />}
+        to={<StudentChip name={props.username} level={1} />}
+        subject={"The End of the Line... So Far"}
+        videoUrl="https://codespells-org.s3.amazonaws.com/NexusVideos/e3.mp4"
+        text={
+          <>
+            <Typography paragraph>
+              Thanks so much for playing our game so far! We are still working on it, but if you'd like to support the project, here are some options: 
+            </Typography>
+            <Typography paragraph>
+              Links  
+            </Typography>
+            <Typography paragraph>
+            
+            </Typography>
+          </>
+        }
+      />
+    </div>
   )
 }
 
