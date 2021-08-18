93ff509e8255269fa5df9883640edf1e7c0923b5
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2021-08-18 15:45:18 -0700

    not working yet, but outline for how gui should work

--- nexus-client/Installer/what-to-do-next.rkt
+++ nexus-client/Installer/what-to-do-next.rkt
@@ -11,7 +11,7 @@
   ; Display GUI
   (send frame show #t)
   
-  ; Check to see if there's anything installed (Check for Versions folder)i
+  ; Main panel within GUI
   (define a-panel (new vertical-panel%
                      (parent frame)
                      (vert-margin 25)
@@ -21,18 +21,41 @@
   
   (define msg (new message% [parent a-panel]
                             [auto-resize #t]
-                            [label "Something"]))
+                            [label ""]))
 
-  (when (not (directory-exists? "Versions"))
-    (send msg set-label "No Versions found")
-    (new button% [parent a-panel]
+(define (update-button)
+  (new button% [parent a-panel]
                  [label "Download Latest Version"]
                  [callback (lambda (button event)
-                              (send msg set-label "Downloading..."))])
-  ) 
+                              (send msg set-label "Downloading..."))]))
+
+(define (play-button)
+  (new button% [parent a-panel]
+                 [label "Play the Game"]
+                 [callback (lambda (button event)
+                              (send msg set-label "Launching..."))]))
+
+;This should instead scrape version # from a file and check it against current version?
+(define (latest-version? str)
+  (string=? str "0.1"))
+
+  ; Check to see if there's anything installed (Check for Versions folder)i
   ; If not, download latest version and put in Versions folder
+  (when (not (directory-exists? "Versions"))
+    (send msg set-label "No Versions found")
+    (update-button)
+    )
 
  ; If there is something installed,
+  (when (directory-exists? "Versions")
+    (if (not (latest-version? "0.1")
+        (let () 
+          (send msg set-label "New version available")
+          (update-button))
+        (let ()
+          (send msg set-label "Up-to-date!")
+          (play-button)
+          ))))
  ; check to see if it's version is current
  ; If not, update! If it is, offer a Launch button. 
   
