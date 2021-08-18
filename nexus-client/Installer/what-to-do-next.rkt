92a93c692e837e3248ebbd7028ed3764c610cd5d
Stephen Foster <stephen@thoughtstem.com>
2021-08-18 11:28:15 -0700

    working on the what-to-do-next GUI that will help user install latest version of Orb World

--- nexus-client/Installer/what-to-do-next.rkt
+++ nexus-client/Installer/what-to-do-next.rkt
@@ -1,5 +1,40 @@
 ; This file gets hosted at https://codespells-org.s3.amazonaws.com/Nexus/Installer/what-to-do-next.rkt
 ; Gets fetched & eval-ed by main.rkt
 (let ()
-  (displayln "Do this, then that")
+  (local-require racket/gui)
+  
+  ; Main window
+  (define frame (new frame% [label "CodeSpells"]
+                            [width 600]
+                            [height 300]))
+  
+  ; Display GUI
+  (send frame show #t)
+  
+  ; Check to see if there's anything installed (Check for Versions folder)i
+  (define a-panel (new vertical-panel%
+                     (parent frame)
+                     (vert-margin 25)
+                     (horiz-margin 25)
+                     (alignment '(center center))
+                     (style (list 'border))))
+  
+  (define msg (new message% [parent a-panel]
+                            [auto-resize #t]
+                            [label "Something"]))
+
+  (when (not (directory-exists? "Versions"))
+    (send msg set-label "No Versions found")
+    (new button% [parent a-panel]
+                 [label "Download Latest Version"]
+                 [callback (lambda (button event)
+                              (send msg set-label "Downloading..."))])
+  ) 
+  ; If not, download latest version and put in Versions folder
+
+ ; If there is something installed,
+ ; check to see if it's version is current
+ ; If not, update! If it is, offer a Launch button. 
+  
+  (displayln "Is this taking changes?")
   )
