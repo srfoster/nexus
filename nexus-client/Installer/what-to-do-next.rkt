130141bd3df1bdbe6c366d00d8f59d8f4199a7f2
Lindsey Handley <lindseyhandley@gmail.com>
2021-08-24 14:50:00 -0700

    fixed exe icons to codespells icon

--- nexus-client/Installer/what-to-do-next.rkt
+++ nexus-client/Installer/what-to-do-next.rkt
@@ -31,47 +31,6 @@
   (define msg (new message% [parent a-panel]
                             [auto-resize #t]
                             [label ""]))
-<<<<<<< HEAD
-
-(define (update-button)
-  (new button% [parent a-panel]
-                 [label "Download Latest Version"]
-                 [callback (lambda (button event)
-                              (send msg set-label "Downloading..."))]))
-
-(define (play-button)
-  (new button% [parent a-panel]
-                 [label "Play the Game"]
-                 [callback (lambda (button event)
-                              (send msg set-label "Launching..."))]))
-
-;This should instead scrape version # from a file and check it against current version?
-(define (latest-version? str)
-  (string=? str "0.1"))
-
-  ; Check to see if there's anything installed (Check for Versions folder)i
-  ; If not, download latest version and put in Versions folder
-  (when (not (directory-exists? "Versions"))
-    (send msg set-label "No Versions found")
-    (update-button)
-    )
-
- ; If there is something installed,
-  (when (directory-exists? "Versions")
-    (if (not (latest-version? "0.1")
-        (let () 
-          (send msg set-label "New version available")
-          (update-button))
-        (let ()
-          (send msg set-label "Up-to-date!")
-          (play-button)
-          ))))
- ; check to see if it's version is current
- ; If not, update! If it is, offer a Launch button. 
-  
-  (displayln "Is this taking changes?")
-  )
-=======
 
   (define (find-dl-size url)
     (local-require net/http-easy)
@@ -197,4 +156,3 @@
   ; check to see if it's version is current
   ; If not, update! If it is, offer a Lunch button. 
 )
->>>>>>> cc5647cf85cb5b9cddc907e5d57e72aff20f4f19
