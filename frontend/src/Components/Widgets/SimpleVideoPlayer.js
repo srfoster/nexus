d1e76add5d1825ac33f8ec1d00a76f333c6e253e
Lindsey Handley <lindseyhandley@gmail.com>
2021-09-03 15:50:34 -0700

    refactored out video player functionality into separate file, added elapsed time over duration counter

---
+++ frontend/src/Components/Widgets/SimpleVideoPlayer.js
@@ -0,0 +1,191 @@
+import { useState, useEffect, useRef } from 'react';
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import ReactPlayer from 'react-player'
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
+import Grid from '@material-ui/core/Grid';
+import IconButton from "@material-ui/core/IconButton";
+import Slider from "@material-ui/core/Slider";
+import Tooltip from "@material-ui/core/Tooltip";
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+	gridItems: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	controlIcons: {
+		color: "#777",
+		fontSize: 50,
+		transform: "scale(0.9)",
+		"&:hover": {
+			color: "#fff",
+			transform: "scale(1)",
+		},
+	}
+}))
+
+function PlayerControls({ onPlayPause, playing, played, onSeek, onSeekMouseDown, onSeekMouseUp, elapsedTime, totalDuration }) {
+  const classes = useStyles();
+
+	return (<>
+		<div style={{ paddingRight: 20, }}>
+			<Grid container direction="row">
+				<Grid item xs={2}>
+					<IconButton
+						onClick={onPlayPause}
+						className={classes.controlIcons}
+						aria-label="play"
+					>
+						{playing ? (
+							<PauseIcon fontSize="inherit" />
+						) : (
+							<PlayArrowIcon fontSize="inherit" />
+						)}
+					</IconButton>
+				</Grid>
+				<Grid item xs={8} className={classes.gridItems}>
+					<PrettoSlider
+						min={0}
+						max={100}
+						value={played * 100}
+						ValueLabelComponent={(props) => <ValueLabelComponent {...props} value={elapsedTime} />}
+						onChange={onSeek}
+						onMouseDown={onSeekMouseDown}
+						onChangeCommitted={onSeekMouseUp}
+					/>
+				</Grid>
+				<Grid item xs={2} className={classes.gridItems}>
+					<Typography>{elapsedTime}/{totalDuration}</Typography>
+				</Grid>
+			</Grid>
+		</div>
+	</>)
+}
+
+const PrettoSlider = withStyles({
+  root: {
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit",
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+function SimpleVideoPlayer(props) {
+  const playerRef = useRef(null);
+  const [playing, setPlaying] = useState(false)
+
+  const [state, setState] = useState({
+    played: 0,
+    duration: 0,
+    seeking: false,
+  });
+
+  const format = (sec) => {
+    if(isNaN(sec)){
+      return '00:00'
+    }
+
+    const date = new Date(sec * 1000)
+    const hh = date.getUTCHours()
+    const mm = date.getUTCMinutes()
+    const ss = date.getUTCSeconds().toString().padStart(2,"0")
+    if(hh){
+      return `${hh}:${mm.toString().padStart(2,"0")}:${ss}`
+    }
+
+    return `${mm}:${ss}`
+  }
+
+  const handlePlayPause = () => {
+    setPlaying(!playing);
+  }
+
+  const handleProgress = (changeState) => {
+    if(!state.seeking){
+      setState({ ...state, ...changeState })
+    }
+  }
+
+  const handleSeekChange = (e, newVal) => {
+    setState({...state, played: parseFloat(newVal / 100)})
+  }
+
+  const handleSeekMouseDown = (e) => {
+    setState({...state, seeking:true})
+  }
+
+  const handleSeekMouseUp = (e, newVal) => {
+    setState({...state, seeking: false})
+    playerRef.current.seekTo(newVal / 100)
+  }
+
+  const currentTime = playerRef.current ? playerRef.current.getCurrentTime() : '00:00'
+  const duration = playerRef.current? playerRef.current.getDuration() : '00:00'
+  const elapsedTime = format(currentTime)
+  const totalDuration = format(duration)
+
+  return (
+    <>
+      <div style={{ backgroundColor: "black" }}>
+        <ReactPlayer
+          ref={playerRef}
+          width={"100%"}
+          url={props.videoUrl}
+          controls={false}
+          playing={playing}
+          style={{}}
+          progressInterval={100}
+          onProgress={handleProgress}
+          onEnded={() => {
+            props.setVideoFinished(true)
+          }}
+        />
+        <PlayerControls
+          onPlayPause={handlePlayPause}
+          playing={playing}
+          played={state.played}
+          onSeek={handleSeekChange}
+          onSeekMouseDown={handleSeekMouseDown}
+          onSeekMouseUp={handleSeekMouseUp}
+          elapsedTime={elapsedTime}
+          totalDuration={totalDuration}
+        />
+      </div>
+    </>
+  )
+}
+
+export default SimpleVideoPlayer;
