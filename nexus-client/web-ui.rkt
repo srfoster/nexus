d29c63954e4a182ee4e147524d25b6f96a0ca744
Stephen Foster <stephen@thoughtstem.com>
2021-09-23 16:40:46 -0700

    more functional api stuff

--- nexus-client/web-ui.rkt
+++ nexus-client/web-ui.rkt
@@ -54,26 +54,71 @@ Begin functional API for Voxel Worlds:
 
 (struct builder (t p w d h c))
 
+;Constructors
+
+(define (empty w [d w] [h w])
+  (builder 'empty (vec 0 0 0) w d h #f))
+
+(define (hexagon w d h)
+  (builder 'empty (vec 0 0 0) w d h #f))
+
+(define (sphere r)
+  (builder 'sphere (vec 0 0 0) (* 2 r) (* 2 r) (* 2 r) #f))
+
+;Getters
+
+(define width builder-w)
+(define depth builder-d)
+(define height builder-h)
+
+;Setters
+
+;More of a util, not for users.
 (define (builder-translate b v)
   (struct-copy builder b [p (+vec v (builder-p b))]))
 
+(define (scale s b)
+  (struct-copy builder b 
+    [p (*vec s (builder-p b))]
+    [w (* s (builder-w b))]
+    [d (* s (builder-d b))]
+    [h (* s (builder-h b))]
+    [c (and 
+        (builder-c b)
+        (map (curry scale s) (builder-c b)))]))
+
+(define (rotate b)
+  b)
+
+;Combinators
 (define (above . bs)
   (combine-all above_ bs))
 
-(define (beside . bs)
-  (combine-all beside_ bs))
+(define (beside/wide . bs)
+  (combine-all beside/wide_ bs))
 
-(define (before . bs)
-  (combine-all before_ bs))
+(define beside beside/wide)
+
+(define (beside/deep . bs)
+  (combine-all beside/deep_ bs))
+
+(define (overlay . bs)
+  (combine-all overlay_ bs))
 
 (define (combine-all f bs)
   (if (= (length bs) 1)
       (first bs)
-      ;(above_ (first bs) (apply above (rest bs)))
       (combine-all f (cons
                       (f (first bs) (second bs))
-                      (drop bs 2)))
-      ))
+                      (drop bs 2)))))
+
+(define (overlay_ b1 b2)
+  (builder 'overlay 
+           (vec 0 0 0) 
+           (max (builder-w b1) (builder-w b2))
+           (max (builder-d b1) (builder-d b2))
+           (max (builder-h b1) (builder-h b2))
+           (list b1 b2)))
 
 (define (above_ b1 b2)
   (define recenter (vec 0
@@ -90,13 +135,13 @@ Begin functional API for Voxel Worlds:
            (list (builder-translate b1 (+vec recenter (vec 0 0 (/ (builder-h b1) 2)))) 
                  (builder-translate b2 (+vec recenter (vec 0 0 (/ (builder-h b2) -2)))))))
 
-(define (beside_ b1 b2)
+(define (beside/wide_ b1 b2)
   (define recenter (vec (/ (- 
                             (builder-w b2) 
                             (builder-w b1))
                            2)
                         0 0 ))
-  (builder 'beside 
+  (builder 'beside/wide
            (vec 0 0 0) 
            (+ (builder-w b1) (builder-w b2))
            (max (builder-d b1) (builder-d b2))
@@ -104,14 +149,14 @@ Begin functional API for Voxel Worlds:
            (list (builder-translate b1 (+vec recenter (vec (/ (builder-w b1) 2) 0 0)))
                  (builder-translate b2 (+vec recenter (vec (/ (builder-w b2) -2) 0 0))))))
 
-(define (before_ b1 b2)
+(define (beside/deep_ b1 b2)
   (define recenter (vec 0
                         (/ (- 
                             (builder-d b2) 
                             (builder-d b1))
                            2)
                         0 ))
-  (builder 'before 
+  (builder 'beside/deep
            (vec 0 0 0) 
            (max (builder-w b1) (builder-w b2))
            (+ (builder-d b1) (builder-d b2))
@@ -119,23 +164,21 @@ Begin functional API for Voxel Worlds:
            (list (builder-translate b1 (+vec recenter (vec 0 (/ (builder-d b1) 2) 0)))
                  (builder-translate b2 (+vec recenter (vec 0 (/ (builder-d b2) -2) 0))))))
 
-(define (empty w d h)
-  (builder 'empty (vec 0 0 0) w d h #f))
 
-(define (hexagon w d h)
-  (builder 'empty (vec 0 0 0) w d h #f))
-
-(define (sphere r)
-  (builder 'sphere (vec 0 0 0) (* 2 r) (* 2 r) (* 2 r) #f))
-
-(define (build-builder b [at (current-location)])
+;Renderer
+(define (build b [at (current-location)])
   (define at-rel (+vec at (builder-p b)))
 
   (match (builder-t b)
     ['sphere (build-sphere at-rel (/ (builder-w b) 2))] 
     ['empty  (void)] 
-    [else (map (curryr build-builder at-rel) 
+    [else (map (curryr build at-rel) 
                (builder-c b))]))
+
+
+
+
+;Side-effectful functions we are building our effect-free API atop of
 
 (define/contract (build-sphere pos r)
   ;If the radius is too big, you end up crashing the game.
@@ -187,6 +230,9 @@ Begin functional API for Voxel Worlds:
   
   unreal-response)
 
+
+
+;For puzzle checking
 (define (check-voxels . vecs)
   ;Why string-join?  Why can't ->unreal-value handle a list?
   (define unreal-response
